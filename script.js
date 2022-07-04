5e89cbf157a1324b146eea2249cbfbd678187954
williamphk <65807958+williamphk@users.noreply.github.com>
2022-07-04 22:55:44 +0800

    allow direction change

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-const createShip = (shipName, shipLength) => {
+const createShip = (shipName, shipLength, direction, group) => {
   if (shipLength > 4 || shipLength < 1) {
     throw new Error("Wrong Length");
   } else
     return {
       shipName: shipName,
       shipLength: shipLength,
+      direction: direction,
+      group: group,
       array: [...Array(shipLength)],
       hit(hitPosition) {
         if (hitPosition >= this.array.shipLength || hitPosition < 0) {
@@ -18,16 +20,27 @@ const createShip = (shipName, shipLength) => {
     };
 };
 
-const ship1 = createShip("ship1", 1);
-const ship2 = createShip("ship2", 1);
-const ship3 = createShip("ship3", 1);
-const ship4 = createShip("ship4", 1);
-const ship5 = createShip("ship5", 2);
-const ship6 = createShip("ship6", 2);
-const ship7 = createShip("ship7", 2);
-const ship8 = createShip("ship8", 3);
-const ship9 = createShip("ship9", 3);
-const ship10 = createShip("ship10", 4);
+const selfShip1 = createShip("ship1", 1, "horizontal", "self");
+const selfShip2 = createShip("ship2", 1, "horizontal", "self");
+const selfShip3 = createShip("ship3", 1, "horizontal", "self");
+const selfShip4 = createShip("ship4", 1, "horizontal", "self");
+const selfShip5 = createShip("ship5", 2, "horizontal", "self");
+const selfShip6 = createShip("ship6", 2, "horizontal", "self");
+const selfShip7 = createShip("ship7", 2, "horizontal", "self");
+const selfShip8 = createShip("ship8", 3, "horizontal", "self");
+const selfShip9 = createShip("ship9", 3, "horizontal", "self");
+const selfShip10 = createShip("ship10", 4, "horizontal", "self");
+
+const rivalShip1 = createShip("ship1", 1, "horizontal", "rival");
+const rivalShip2 = createShip("ship2", 1, "horizontal", "rival");
+const rivalShip3 = createShip("ship3", 1, "horizontal", "rival");
+const rivalShip4 = createShip("ship4", 1, "horizontal", "rival");
+const rivalShip5 = createShip("ship5", 2, "horizontal", "rival");
+const rivalShip6 = createShip("ship6", 2, "horizontal", "rival");
+const rivalShip7 = createShip("ship7", 2, "horizontal", "rival");
+const rivalShip8 = createShip("ship8", 3, "horizontal", "rival");
+const rivalShip9 = createShip("ship9", 3, "horizontal", "rival");
+const rivalShip10 = createShip("ship10", 4, "horizontal", "rival");
 
 const createGameboard = () => {
   return {
@@ -67,7 +80,10 @@ const createGameboard = () => {
       }
     },
     receiveAttack(x, y) {
-      if (this.board[x][y] === "ship") {
+      if (
+        this.board[x][y] === "ship" ||
+        (typeof this.board[x][y] === "object" && this.board[x][y] != null)
+      ) {
         this.board[x][y] = "hit";
       } else if (this.board[x][y] === null) {
         this.board[x][y] = "missed";
@@ -93,27 +109,27 @@ const createGameboard = () => {
 let boardSelf = createGameboard();
 let boardRival = createGameboard();
 
-boardRival.placeShip(0, 0, ship1, "horizontal");
-boardRival.placeShip(1, 0, ship2, "horizontal");
-boardRival.placeShip(2, 0, ship3, "horizontal");
-boardRival.placeShip(3, 0, ship4, "horizontal");
-boardRival.placeShip(4, 0, ship5, "horizontal");
-boardRival.placeShip(5, 0, ship6, "horizontal");
-boardRival.placeShip(6, 0, ship7, "horizontal");
-boardRival.placeShip(7, 0, ship8, "horizontal");
-boardRival.placeShip(8, 0, ship9, "horizontal");
-boardRival.placeShip(9, 0, ship10, "horizontal");
+boardRival.placeShip(0, 0, selfShip1, "horizontal");
+boardRival.placeShip(0, 3, selfShip2, "horizontal");
+boardRival.placeShip(0, 6, selfShip3, "horizontal");
+boardRival.placeShip(0, 9, selfShip4, "horizontal");
+boardRival.placeShip(2, 1, selfShip5, "horizontal");
+boardRival.placeShip(2, 7, selfShip6, "horizontal");
+boardRival.placeShip(3, 4, selfShip7, "horizontal");
+boardRival.placeShip(6, 1, selfShip8, "horizontal");
+boardRival.placeShip(6, 6, selfShip9, "horizontal");
+boardRival.placeShip(8, 3, selfShip10, "horizontal");
 
-boardSelf.placeShip(0, 0, ship1, "horizontal");
-boardSelf.placeShip(1, 0, ship2, "horizontal");
-boardSelf.placeShip(2, 0, ship3, "horizontal");
-boardSelf.placeShip(3, 0, ship4, "horizontal");
-boardSelf.placeShip(4, 0, ship5, "horizontal");
-boardSelf.placeShip(5, 0, ship6, "horizontal");
-boardSelf.placeShip(6, 0, ship7, "horizontal");
-boardSelf.placeShip(7, 0, ship8, "horizontal");
-boardSelf.placeShip(8, 0, ship9, "horizontal");
-boardSelf.placeShip(9, 0, ship10, "horizontal");
+boardSelf.placeShip(0, 0, rivalShip1, "horizontal");
+boardSelf.placeShip(0, 3, rivalShip2, "horizontal");
+boardSelf.placeShip(0, 6, rivalShip3, "horizontal");
+boardSelf.placeShip(0, 9, rivalShip4, "horizontal");
+boardSelf.placeShip(2, 1, rivalShip5, "horizontal");
+boardSelf.placeShip(2, 7, rivalShip6, "horizontal");
+boardSelf.placeShip(3, 4, rivalShip7, "horizontal");
+boardSelf.placeShip(6, 1, rivalShip8, "horizontal");
+boardSelf.placeShip(6, 6, rivalShip9, "horizontal");
+boardSelf.placeShip(8, 3, rivalShip10, "horizontal");
 
 const createPlayer = (name) => {
   return { name };
@@ -140,21 +156,10 @@ battleCellContentRival.forEach((cell, index) => {
   });
 });
 
-/*const gameLogic = (() => {
+const gameLogic = (() => {
   let turn = player1.name;
-  for (let x = 0; x < 10; x++) {
-    for (let y = 0; y < 10; y++) {
-      if (typeof boardSelf.board[x][y] === "object" && boardSelf.board[x][y] != null) {
-        let cell = document.querySelector(
-          `[class$="battle-cell-content battle-cell-content__self"][data-x="${x}"][data-y="${y}"]`
-        );
-        let shipObj = boardSelf.board[x][y];
-        cell.style.width = `${shipObj.shipLength * 32 + (shipObj.shipLength - 1) * 1}px`;
-      }
-    }
-  }
   return { turn: turn };
-})();*/
+})();
 
 const AIMove = (x, y) => {
   if (boardSelf.isHit(x, y) || boardSelf.isMiss(x, y)) {
@@ -164,7 +169,12 @@ const AIMove = (x, y) => {
     let cell = document.querySelector(
       `[class$="battle-cell-content battle-cell-content__self"][data-x="${x}"][data-y="${y}"]`
     );
-    cell.innerHTML = "。";
+    if (boardSelf.isHit(x, y)) {
+      cell.innerHTML = "H";
+      cell.style.backgroundColor = "red";
+    } else if (boardSelf.isMiss(x, y)) {
+      cell.innerHTML = "M";
+    }
     gameLogic.turn = player1.name;
   }
 };
@@ -176,27 +186,67 @@ const randomY = () => {
   return Math.floor(Math.random() * 10);
 };
 
-function allowDrop(ev) {
-  ev.preventDefault();
-}
-
 function drag(ev) {
   ev.dataTransfer.setData("target", ev.target.id);
+  ev.dataTransfer.setData("target-length", ev.target.dataset.length);
   ev.dataTransfer.setData("srcX", ev.path[1].dataset.x);
   ev.dataTransfer.setData("srcY", ev.path[1].dataset.y);
 }
 
+function allowDrop(ev) {
+  ev.preventDefault();
+  ev.dataTransfer.dropEffect = "move";
+}
+
 function drop(ev) {
   let data = ev.dataTransfer.getData("target");
+  let dataLength = ev.dataTransfer.getData("target-length");
   let originalLocationX = Number(ev.dataTransfer.getData("srcX"));
   let originalLocationY = Number(ev.dataTransfer.getData("srcY"));
   let targetX = Number(ev.target.dataset.x);
   let targetY = Number(ev.target.dataset.y);
   ev.preventDefault();
-  ev.target.appendChild(document.getElementById(data));
-  //console.log(ev.target);
   let shipObj = boardSelf.board[originalLocationX][originalLocationY];
-  boardSelf.removeShip(originalLocationX, originalLocationY, shipObj, "horizontal");
-  boardSelf.placeShip(targetX, targetY, shipObj, "horizontal");
-  console.log(boardSelf.board);
+  if (shipObj.direction === "horizontal") {
+    if (targetY > 9 - dataLength + 1) return;
+    else {
+      ev.target.appendChild(document.getElementById(data));
+      boardSelf.removeShip(originalLocationX, originalLocationY, shipObj, "horizontal");
+      boardSelf.placeShip(targetX, targetY, shipObj, "horizontal");
+    }
+  } else if (shipObj.direction === "vertical") {
+    if (targetX > 9 - dataLength + 1) return;
+    else {
+      ev.target.appendChild(document.getElementById(data));
+      boardSelf.removeShip(originalLocationX, originalLocationY, shipObj, "vertical");
+      boardSelf.placeShip(targetX, targetY, shipObj, "vertical");
+    }
+  }
+}
+
+function shipClick(event) {
+  let shipX = Number(event.path[1].dataset.x);
+  let shipY = Number(event.path[1].dataset.y);
+  let shipObj = boardSelf.board[shipX][shipY];
+  let shipLength = Number(event.target.dataset.length);
+  let shipSize = 32 * shipLength + shipLength - 1;
+  if (shipObj.direction === "horizontal") {
+    if (shipX > 9 - shipLength + 1) return;
+    else {
+      event.target.style.width = "32px";
+      event.target.style.height = shipSize + "px";
+      boardSelf.removeShip(shipX, shipY, shipObj, "horizontal");
+      boardSelf.placeShip(shipX, shipY, shipObj, "vertical");
+      shipObj.direction = "vertical";
+    }
+  } else if (shipObj.direction === "vertical") {
+    if (shipY > 9 - shipLength + 1) return;
+    else {
+      event.target.style.width = shipSize + "px";
+      event.target.style.height = "32px";
+      boardSelf.removeShip(shipX, shipY, shipObj, "vertical");
+      boardSelf.placeShip(shipX, shipY, shipObj, "horizontal");
+      shipObj.direction = "horizontal";
+    }
+  }
 }
